26f18ccabc1615102f54b9844cc485e279b4bf90
modify879 <modify879@gmail.com>
2024-03-19 22:30:46 +0900

    Initial commit

---
+++ src/main/java/com/jsm/bluemarble/BlueMarbleApplication.java
@@ -0,0 +1,13 @@
+package com.jsm.bluemarble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueMarbleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueMarbleApplication.class, args);
+    }
+
+}
